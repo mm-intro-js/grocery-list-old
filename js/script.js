3e0628b6f645d66ab61e30bdaae6aa9839959327
Jim Wollin <jwollin001@myHennepinTech.edu>
2016-02-02 14:53:50 -0600

    assignment 3 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+var listItem = document.getElementsByTagName("li");
+for (i = 0; i < listItem.length; i++) 
+{
+    
+    ListItems = listItem[i];
+    console.log(ListItems);
+    
+}
+
+    var listItem = document.getElementById("grocery-list");
+   
+    while(listItem.hasChildNodes())
+   {
+        listItem.removeChild(listItem.firstChild);
+   }
+  
+
+
+
+var newListItem = document.createElement("li");
+var listItemValue = document.createTextNode("Bought all the groceries.");
+var position = document.getElementsByTagName("ul")[0];
+newListItem.appendChild(listItemValue);
+position.appendChild(newListItem);
+
 

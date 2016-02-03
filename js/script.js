8068ad93fccd3b9f44ffecf30ef2d32d52b49e1a
olidi10 <mmutanga@yahoo.fr>
2016-02-03 09:17:02 -0600

    hw 3 glocery list

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
+//geting all the items from grocery-list.(Gives an array of items)
+var glocery - list = document.getElementsByTagName("li");
 
+
+//grocery-list items.
+var glocery - list = ["Milk", "Cheese", "Bread", "Ice-cream"];
+
+
+//logging the items into console.
+console.log(glocery - list);
+
+
+//looping through the list
+function gloceryitems() {
+    for (var i = 0; i < grocery - list.length; i++)
+        console.log()
+};
+
+// removing all items in the list
+function removeGloceryList {
+    var grocery - list = removeElementById(glocery - list)
+};
+
+alert = ("Bought all the gloceries")

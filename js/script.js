9505d688f6acf2d6613cd6ec13be76d54eba92a9
carlbrost <brostmech@gmail.com>
2016-02-03 19:14:59 -0600

    "PLZ FREAKIN WORK"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
+function gclear()
+ {
 
+var glist = document.getElementById("grocery-list"); 
+var GroceryArray = document.getElementsByTagName("li"); 
+
+     
+     //main loop set up
+     
+while(GroceryArray.length > 0) 
+{ 
+    console.log(GroceryArray[0].textContent);
+    glist.removeChild(GroceryArray[0]);
+    
+    
+    
+}
+ 
+     /*
+ //POST ISSUE it's not working  
+     Should be posting
+     var newlist = document.createElement("li");
+     
+     var newlistitem document.createTextNode("Groceries Aquired");
+     
+     newlist.appendChild(newlistitem);
+     
+     */
+     
+
+ }
+gclear();

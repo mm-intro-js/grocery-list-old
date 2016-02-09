06af753fca657d4b3a5155306e1889a130e077a5
Ryan Freemark <freemark21@gmail.com>
2016-02-08 18:33:19 -0600

    Modified JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+var ul = document.getElementById("grocery-list");
+var liElements = ul.getElementsByTagName('li');
+var len = liElements.length;
 
+if (len >= 1) {
+    for (i = 0; i < len; i++) {
+        console.log(liElements[i].textContent);
+    }
+    while (len > 0) {
+        ul.removeChild(liElements[0]);
+    }
+    var newEl = document.createElement('li');
+    var newTxt = document.createTextNode("Bought all the groceries");
+    newEl.appendChild(newTxt);
+    ul.appendChild(newEl);
+}
